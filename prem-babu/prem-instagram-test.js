833e12d15e49d69fce83a35f28778d02d1a6f977
प्रेम बाबू <144128294+prem-project3608@users.noreply.github.com>
2025-06-11 16:41:37 +0530

    Update prem-instagram-test.js

--- prem-babu/prem-instagram-test.js
+++ prem-babu/prem-instagram-test.js
@@ -2,18 +2,18 @@ const prem_instagram = require("./prem-instagram")
 
 describe("prem_instagram", ()=>{
     test("Should successfully get multiple videos/images from the post", async ()=>{
-        await expect(instagramGetUrl("https://www.instagram.com/p/CMAMhvgsVal")).toResolve()
+        await expect(prem_instagram("https://www.instagram.com/p/CMAMhvgsVal")).toResolve()
     })
     
     test("Should successfully get a single video from the post", async ()=>{
-        await expect(instagramGetUrl("https://www.instagram.com/tv/CdmYaq3LAYo/")).toResolve()
+        await expect(prem_instagram("https://www.instagram.com/tv/CdmYaq3LAYo/")).toResolve()
     })
 
     test("Should successfully get a single image from the post", async ()=>{
-        await expect(instagramGetUrl("https://www.instagram.com/uniaomalacos.ofc/p/C6bkGqBuGwD/?__d=1%2F")).toResolve()
+        await expect(prem_instagram("https://www.instagram.com/uniaomalacos.ofc/p/C6bkGqBuGwD/?__d=1%2F")).toResolve()
     })
 
     test("Should give a error on invalid link", async()=>{
-        await expect(instagramGetUrl("https://www.instagram.com/uniaomalacos.ofc/p/C6bkGq323BuGwD/")).toReject()
+        await expect(prem_instagram("https://www.instagram.com/uniaomalacos.ofc/p/C6bkGq323BuGwD/")).toReject()
     })
 })
